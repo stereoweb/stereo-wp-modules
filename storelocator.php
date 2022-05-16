07da5dff5ec8a5d045c65d5f27026ea8ba8000e2
David Boissonnault <david@stereo.ca>
2022-05-16 13:20:58 -0400

    WIP

---
+++ storelocator.php
@@ -0,0 +1,7 @@
+<?php
+
+// Installer script. 
+$module = 'storelocator';
+$mpath = __DIR__.'/'.$module;
+$this->copy($mpath.'/includes/storelocator.php',$this->theme_path.'/includes/storelocator.php');
+$this->append($mpath.'/functions.php',$this->theme_path.'/functions.php');
